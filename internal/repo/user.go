1f42e5a282a0d7c0f2f2f750192f2069aa084106
gingray <gingray.dev@gmail.com>
2026-03-01 14:28:37 +0100

    rename model to entity

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -3,13 +3,13 @@ package repo
 import (
 	"context"
 
-	"github.com/gingray/go-template/internal/model"
+	"github.com/gingray/go-template/internal/entity"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserRepo interface {
-	GetUsers(ctx context.Context) ([]model.User, error)
+	GetUsers(ctx context.Context) ([]entity.User, error)
 }
 
 type User struct {
@@ -20,12 +20,12 @@ func NewUserRepo(pgPool *pgxpool.Pool) *User {
 	return &User{pgPool: pgPool}
 }
 
-func (u *User) GetUsers(ctx context.Context) ([]model.User, error) {
+func (u *User) GetUsers(ctx context.Context) ([]entity.User, error) {
 	res, err := u.pgPool.Query(ctx, "SELECT * FROM users")
 	if err != nil {
 		return nil, err
 	}
-	users, err := pgx.CollectRows(res, pgx.RowToStructByName[model.User])
+	users, err := pgx.CollectRows(res, pgx.RowToStructByName[entity.User])
 	if err != nil {
 		return nil, err
 	}
